7373843d894d41653709f501e5ea6802fe08bc34
Dan French <34012200+pzfrenchy@users.noreply.github.com>
2019-08-16 21:04:55 +0100

    Adding DataAnnotations and validation to Models

--- MvcMovie/Models/Games.cs
+++ MvcMovie/Models/Games.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +12,12 @@ namespace MvcMovie.Models
         public int Id { get; set; }
         [StringLength(50)]
         public string Title { get; set; }
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date, ErrorMessage = "Invalid date")]
         public DateTime ReleaseDate { get; set; }
         [StringLength(20)]
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
